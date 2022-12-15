f50eb88ea1e2d731c0da71c48859671ceba78ce9
Raul Leano <theylooksotired@gmail.com>
2022-12-15 22:39:33 +0100

    Fixed articles

--- base/lib/Post/Post_Ui.php
+++ base/lib/Post/Post_Ui.php
@@ -221,11 +221,6 @@ class Post_Ui extends Ui
                 'name' => Parameter::code('meta_title_page'),
                 'url' => url(''),
             ],
-            'publisher' => [
-                '@type' => 'Organization',
-                'name' => Parameter::code('meta_title_page'),
-                'logo' => Parameter::code('meta_image'),
-            ],
             'datePublished' => $this->object->get('publish_date'),
             'dateModified' => $this->object->get('publish_date'),
         ];
